480c3604e390bd08527d86cd9e42f15b86fc74ff
timze <timze@timzedeMacBook.lan>
2020-02-10 12:22:51 +0800

    fix: 修改header对应上游数据变动
    不再写死dataframe column，改为从json提取

--- nCoV2019/__init__.py
+++ nCoV2019/__init__.py
@@ -2,13 +2,16 @@ import requests,json
 from pandas import DataFrame as DF
 class qq_source:
     
-    def __init__(self):
+    def __init__(self, debug = 0):
         self.data_url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5'
         x = requests.get(self.data_url).json()
         data = json.loads(x['data'])
-        header = ['time','confirm','suspect','dead','heal','confirm_add','suspect_add','dead_add','heal_add']
-        self.chinaDayList = DF([ list(i.values()) for i in data['chinaDayList'] ],columns=header[:5])
-        self.chinaDayADD = DF([ list(i.values()) for i in data['chinaDayAddList'] ],columns=header[:5])
+        if debug: self.data = data
+        a = data['areaTree'][0]['today'].keys()
+        b= [ 'total_' + i for i in data['areaTree'][0]['total'].keys()]
+        self.__header = ['city'] + b + list(a)
+        self.chinaDayList = DF([ i.values() for i in data['chinaDayList']],columns=data['chinaDayList'][0].keys())
+        self.chinaDayADD = DF([ i.values() for i in data['chinaDayAddList']],columns=data['chinaDayAddList'][0].keys())
         print(f'截止 {data["lastUpdateTime"]}; 2019nCoV 已蔓延 {len(data["areaTree"])} 个国家/地区')
         print(f'中国累计 {data["chinaTotal"]["confirm"]} 例确诊，自昨日00:00新增{data["chinaAdd"]["confirm"]}')
     
@@ -17,8 +20,8 @@ class qq_source:
         self.area_dict = {} #记录全部地区
         self.total_rec = [self._detail_area(i) for i in areaTree] #解析全部记录
         self.all_area = set(self.area_dict.keys()) # 所有的地名
-        self.global_area = DF(self._country(areaTree),columns=['city'] + header[1:]) # 全球感染情况
-        self.china = DF(self._country(areaTree[0]['children']),columns=['city'] + header[1:]) # 国内感染情况
+        self.global_area = DF(self._country(areaTree),columns=['country'] + self.__header[1:]) # 全球感染情况
+        self.china = DF(self._country(areaTree[0]['children']),columns = self.__header) # 国内感染情况
         
     def _city_row(self,city):
         name = city['name']
@@ -34,8 +37,7 @@ class qq_source:
         if sp:
             for city in area['children']:
                 city_row = city_row + self._detail_area(city)
-        header = ['city','confirm','suspect','dead','heal','confirm_add','suspect_add','dead_add','heal_add']
-        df = DF(city_row,columns=header)
+        df = DF(city_row,columns=self.__header)
         self.area_dict[name] = df
         return(city_row)
     
